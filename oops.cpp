17d3b7e1b77ca3c02c59cb477155344f25841156
The-Deepak-Pandey <deepak.pandey@students.iiit.ac.in>
2025-06-13 22:47:28 +0530

    get set done

--- oops.cpp
+++ oops.cpp
@@ -2,16 +2,27 @@
 using namespace std;
 
 class Teacher {
+
+    private:
+        double salary;
+
     public:
         // properties or attributes
         string name;
         string department;
         string subject;
-        double salary;
 
         // methods or member functions
         void changeDepartment(string newDepartment) {
             department = newDepartment;
+        }
+
+        void setSalary(double newSalary) { // setter method
+            salary = newSalary;
+        }
+
+        double getSalary() {  // getter method
+            return salary;
         }
 };
 
@@ -20,9 +31,11 @@ int main(){
     t1.name = "Deepak";
     t1.subject = "C++";
     t1.department = "Computer Science";
-    t1.salary = 25000;
+    
+    t1.setSalary(50000);
 
     cout << "Teacher Name: " << t1.name << endl;
+    cout << "Salary: " << t1.getSalary() << endl;
 
     return 0;
 }
